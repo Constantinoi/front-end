3caba86ef1aad67a22ed3aa943467b04f32f0ede
Constantinoi <pescaalma@gmail.com>
2020-03-22 23:14:14 -0400

    configuração do metodo exclusao

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -15,6 +15,13 @@ export default class Todo extends Component{
         this.state = {description: '', list: []}
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.refresh()
+    }
+    
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+        .then((resp)=> this.setState({...this.state, description: '', list: resp.data}))
     }
     handleChange(e){
         this.setState({...this.state, description: e.target.value })  
@@ -22,8 +29,13 @@ export default class Todo extends Component{
     handleAdd(){
         const description = this.state.description
         axios.post(URL,{ description })
-        .then(resp =>console.log('Funcionou'))
+        .then(resp => this.refresh())
     }
+    handleRemove(todo){
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
+
     render(){
         return(
             <div>
@@ -32,7 +44,9 @@ export default class Todo extends Component{
               description={this.state.description}
               handleChange={this.handleChange}
               handleAdd={this.handleAdd}/>
-              <TodoList/>
+              <TodoList list={this.state.list}
+                handleRemove={this.handleRemove}
+              />
             </div>
         )
     }   
