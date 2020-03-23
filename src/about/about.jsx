af9b48e0302a1006f9613936879c078ffb0bab0e
Constantinoi <pescaalma@gmail.com>
2020-03-22 21:30:27 -0400

    pageheader

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import PageHeader from '../template/pageHeader'
+
 export default props =>(
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'/>
+        <h2>Nossa Historia</h2>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias ad soluta molestias qui natus possimus corrupti accusamus ipsam, ipsum amet dolorem quae nam eius provident quo, reprehenderit impedit. Corrupti, aliquid.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eum velit laudantium culpa magnam quo accusantium placeat magni repellat iste. Laudantium porro cupiditate similique reiciendis aliquam quam inventore repellendus facere?</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eum velit laudantium culpa magnam quo accusantium placeat magni repellat iste. Laudantium porro cupiditate similique reiciendis aliquam quam inventore repellendus facere?</p>
+        
     </div>
 )
